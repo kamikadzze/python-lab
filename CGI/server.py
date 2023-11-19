19bec86a345eb1059084eadbc050be4bd1a41ffb
kamikadzze <39806019+kamikadzze@users.noreply.github.com>
2023-11-19 20:26:41 +0200

    Delete CGI directory

--- CGI/server.py
+++
@@ -1,6 +0,0 @@
-from http.server import  HTTPServer, CGIHTTPRequestHandler
-adders = ("localhost", 5050)
-
-server = HTTPServer(adders, CGIHTTPRequestHandler)
-print("Listenint...")
-server.serve_forever()
